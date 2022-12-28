ae4260e128678ea504f6a50c75fc73e47bfa133e
Al Sabawi <sabawi@gmail.com>
2022-12-28 08:00:15 -0500

    restructuring code

--- bad_coin_flip.py
+++ bad_coin_flip.py
@@ -5,25 +5,6 @@ import numpy as np
 import time
 import array as arr
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        try:
-            arg_in = float(sys.argv[1])
-        except ValueError:
-            print('Argument',sys.argv[1], 'is not a float data type. Try again.')
-            quit()
-
-        bias = float(sys.argv[1])
-        print('Bias = ',bias)
-    else:
-        print ( '*** Usage:',sys.argv[0], '<bias factor as fraction between 0.0 to 1.0>\n')
-        quit()
-
-max_flips = 100000
-states_num = 2
-#flip_stat = arr.array('i',(0 for i in range(0,states_num)))
-np.random.seed(int(time.time()))
-
 def numaric_array(atype,index_start,index_end,init_value):
     return arr.array(atype,(init_value for i in range(index_start,index_end)))
 
@@ -35,13 +16,52 @@ def generate_bias_outcome(b=0.5):
         return 1
     else:
         return 0
+def flip_a_coin(bias):
+    return(generate_bias_outcome(bias))
 
-print('Testing a coin flip', max_flips,'times. \nResults:')
-flip_stat = numaric_array('i',0,states_num,0)
+def flip_a_coin_many_times(bias,flips):
+    states_num = 2 # 2 states, Head and Tail
 
-for i in range(0,max_flips):
-    res = generate_bias_outcome(bias)
-    flip_stat[res]+=1
+    flip_stat = numaric_array('i',0,states_num,0)
+    for i in range(0,flips):
+        res = flip_a_coin(bias)
+        flip_stat[res]+=1
+    return(flip_stat)
 
-print('\tNumber of Heads',flip_stat[1],'(',round(100*flip_stat[1]/max_flips,2),'%)')
-print('\tNumber of Tails',flip_stat[0],'(',round(100*flip_stat[0]/max_flips,2),'%)')
+def print_input_err(inparm):
+    print('Argument',inparm, 'is not a float data type. Try again.')
+
+def get_arguments():   
+    if __name__ == "__main__":
+        if len(sys.argv) > 1:
+            try:
+                arg_in = float(sys.argv[1])
+            except ValueError:
+                print_input_err(sys.argv[1])
+                quit()
+
+            bias = float(sys.argv[1])
+            if(bias>1.0):
+                print_input_err(sys.argv[1])
+                quit()
+
+            print('Bias = ',bias*100,"% for Head")
+        else:
+            print ( '*** Usage:',sys.argv[0], '<bias factor as fraction between 0.0 to 1.0>\n')
+            quit()
+    return(bias)
+
+def run():
+    max_flips = 100000
+    np.random.seed(int(time.time()))
+
+    print('Testing a coin flip', max_flips,'times. \nResults:')
+
+    bias = get_arguments()
+
+    flip_stat = flip_a_coin_many_times(bias,max_flips)
+
+    print('\tNumber of Heads',flip_stat[1],'(',round(100*flip_stat[1]/max_flips,2),'%)')
+    print('\tNumber of Tails',flip_stat[0],'(',round(100*flip_stat[0]/max_flips,2),'%)')
+
+run()
